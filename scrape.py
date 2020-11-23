0fcd6b7b7b54c86535a9b1a9ab700809f85505d6
Ryan Remaly <remalyryan@gmail.com>
2020-11-23 14:31:27 -0500

    Cleaning commits

--- scrape.py
+++ scrape.py
@@ -1,22 +1,2 @@
-import requests
+import scrapy
 
-url = 'https://www.allsides.com/media-bias/media-bias-ratings'
-
-r = requests.get(url)
-
-print(r.content[:100])
-
-def save_html(html, path):
-    with open(path, 'wb') as f:
-        f.write(html)
-        
-        
-save_html(r.content, 'google_com')
-
-
-def open_html(path):
-    with open(path, 'rb') as f:
-        return f.read()
-    
-    
-html = open_html('google_com')
